4c556465b2dd7396f8a1bfb08d20b5a511401070
Firezac <erick.barros@uol.com.br>
2025-06-26 10:47:23 -0300

    no message

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1750944283|10393554461';
+const CACHE_VERSION = '1750945591|11701984753';
 /** @type {string} */
 const CACHE_PREFIX = 'Fred's Market-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
