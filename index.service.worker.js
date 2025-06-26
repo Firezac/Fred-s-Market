c741cd9d7e2b1aed45ab166366f59f7c7ad1b2ef
Firezac <erick.barros@uol.com.br>
2025-06-26 08:41:07 -0300

    Resize files

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1750936698|2808650887';
+const CACHE_VERSION = '1750938013|4123886458';
 /** @type {string} */
 const CACHE_PREFIX = 'Fred's Market-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
