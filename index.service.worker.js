e75462055697415253a49923bace1f5d9199098a
Firezac <erick.barros@uol.com.br>
2025-06-26 08:56:43 -0300

    Touch Correction

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1750938013|4123886458';
+const CACHE_VERSION = '1750938966|5076269243';
 /** @type {string} */
 const CACHE_PREFIX = 'Fred's Market-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
